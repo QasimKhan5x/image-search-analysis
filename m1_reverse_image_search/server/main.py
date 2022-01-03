a9f3deb4313c8cc32a48b496367b205d573d0ccb
zShan <73174295+zShanCS@users.noreply.github.com>
2022-01-03 18:35:53 +0500

    Update main.py

--- m1_reverse_image_search/server/main.py
+++ m1_reverse_image_search/server/main.py
@@ -8,7 +8,7 @@ from fastapi.staticfiles import StaticFiles
 from inference import get_nn_filepaths
 from PIL import Image
 from pymilvus import connections
-
+import json
 app = FastAPI()
 # connect to Milvus
 connections.connect(host="127.0.0.1", port=19530)
@@ -17,14 +17,16 @@ cur = con.cursor()
 
 
 @app.post("/uploadFile")
-async def create_upload_files(resultLimit: int = Form(...), file: UploadFile = File(...)):
+async def create_upload_files(resultLimit: int = Form(...), img_file: UploadFile = File(...), json_file: UploadFile = File(...)):
     try:
-        img = Image.open(file.file).convert("RGB")
+        img = Image.open(img_file.file).convert("RGB")
         res = get_nn_filepaths(cursor=cur, img=img, topK=resultLimit)
-    except:
+        x = json_file.file.read().decode('utf-8')
+        print(json.loads(x))
+    except Exception as e:
+        print(e)
         res = None
     finally:
         return res
-
 
 app.mount('/', StaticFiles(directory='static', html=True), name='static')
